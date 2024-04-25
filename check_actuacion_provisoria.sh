fb13b872bc1ab60ec6f26347dd8a261a5837dcf5
Matias David Schwerdt <schwerdt.matias@gmail.com>
2024-04-25 17:14:02 -0300

    To fix

--- check_actuacion_provisoria.sh
+++ check_actuacion_provisoria.sh
@@ -2,7 +2,6 @@
 
 cd "$(dirname "$0")" || exit
 
-LOG_FILE="log-file.log"
 timestamp=$(date +"%Y-%m-%d, %H:%M:%S")
 
 # Cargo datos de login
@@ -16,8 +15,8 @@ curl -s 'https://g3w.uns.edu.ar/guarani3w/actuacion_provisoria_examen' \
     -H "Cookie: siu_sess_guarani3w_UNS=${COOKIE}; _ga=GA1.3.268626716.1615387490; _gid=GA1.3.985118667.1618352284" \
 | grep "Sin resultado"
 
-if [ $? -eq 0 ]; then
-    MSG="${timestamp},HAY NOTA DE ARQUI"
+if [ ${?} -eq 0 ]; then # no está funcionando...
+    MSG="${timestamp},HAY NOTA DE ARQUI" 
     telegram "${MSG}" >> "${LOG_FILE}"
 else
     MSG="${timestamp},Nota Arqui: Sin resultado"
