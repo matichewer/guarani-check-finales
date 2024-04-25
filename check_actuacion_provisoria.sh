f106acda33067adb0819e014c263bbd309aeea51
Matias David Schwerdt <schwerdt.matias@gmail.com>
2024-04-25 17:10:47 -0300

    Fix parameters

--- check_actuacion_provisoria.sh
+++ check_actuacion_provisoria.sh
@@ -2,18 +2,15 @@
 
 cd "$(dirname "$0")" || exit
 
-LOG_FILE="/home/wecher/02-disco/01-projects/guarani-check-finales/log-file.log"
+LOG_FILE="log-file.log"
 timestamp=$(date +"%Y-%m-%d, %H:%M:%S")
 
 # Cargo datos de login
 source .env
 
-# ID de Arqui
-ID_MATERIA=5561
-
 # Me logueo en Guarani
 source login_cookie.sh
-login_cookie
+login_cookie "${LU}" "${PASSWORD}"
 
 curl -s 'https://g3w.uns.edu.ar/guarani3w/actuacion_provisoria_examen' \
     -H "Cookie: siu_sess_guarani3w_UNS=${COOKIE}; _ga=GA1.3.268626716.1615387490; _gid=GA1.3.985118667.1618352284" \
